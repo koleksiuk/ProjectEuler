86a8f4d2751b01e64bf31e6a86fc18466e33a470
Konrad Oleksiuk <konole@me.com>
2013-03-29 17:05:21 +0100

    solve Problem 1

---
+++ src/main/scala/ArrayFilter.scala
@@ -0,0 +1,19 @@
+package euler
+
+/*
+ * Task: http://projecteuler.net/problem=1
+ * Description: Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+
+class ArrayFilter(n: Int) {
+  val preparedArray = Array.range(1, n)
+
+  // Public
+  def sum : Int = {
+    return this.filterArray.sum
+  }
+
+  private def filterArray : Array[Int] = {
+    return this.preparedArray.filter(x => (x % 3 == 0 || x %  5 == 0))
+  }
+}
---
+++ src/test/scala/ArrayFilterTest.scala
@@ -0,0 +1,31 @@
+package euler.test
+
+import org.scalatest.FunSpec
+import org.scalatest.BeforeAndAfter
+import euler.ArrayFilter
+
+class ArrayFilterTest extends FunSpec with BeforeAndAfter {
+  var arrFilter: ArrayFilter = _
+
+  describe("ArrayFilter") {
+    before {
+      arrFilter = new ArrayFilter(10)
+    }
+
+    it("should create an array") {
+      assert(arrFilter.preparedArray === Array.range(1,10))
+    }
+
+    it("should return sum of filtered elements") {
+      assert(arrFilter.sum === 23)
+    }
+  }
+
+  describe("Euler task") {
+    it("should return correct value") {
+      arrFilter = new ArrayFilter(1000)
+
+      assert(arrFilter.sum === 233168)
+    }
+  }
+}
